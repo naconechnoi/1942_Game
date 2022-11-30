4f1789841abb67e94ca8d38d0fa6ffde3e26866c
anastasiia <anastasiia_sadova@knu.ua>
2022-11-30 09:13:10 +0100

    main added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from App import App
 from Player import Player
+from RegularEnemy import RegularEnemy
 
 app = App()
 app.add_object(Player(60, 120, 5, 5))
+app.add_object(RegularEnemy())
 
 app.run()
