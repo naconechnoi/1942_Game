8ba6e6f60b1ede346bb13ff92282729862c34d18
cherrnyssheva <117863274+cherrnyssheva@users.noreply.github.com>
2022-11-30 09:07:37 +0100

    added regular enemy class

---
+++ RegularEnemy.py
@@ -0,0 +1,56 @@
+import pyxel
+
+enemy_width = 6
+enemy_height = 6
+enemy_speed = 1
+enemies = []
+
+
+class RegularEnemy:
+
+    def __init__(self, x: int = 50, y: int = 2, color: int = 1):
+        self.x = x
+        self.y = y
+        self.w = enemy_width
+        self.h = enemy_height
+        self.color = color
+        self.timer_offset = pyxel.rndi(0, 59)
+        self.is_alive = True
+        enemies.append(self)
+        self.counter_y = 0
+
+    def move(self):
+
+        if self.counter_y <= 80:
+            if (pyxel.frame_count + self.timer_offset) % 60 < 40:
+                self.x += enemy_speed
+
+            else:
+                self.x -= enemy_speed
+
+            if self.y > pyxel.height - 1:
+                self.is_alive = False
+
+            self.y += enemy_speed
+
+        if self.counter_y > 80:
+            if (pyxel.frame_count + self.timer_offset) % 60 < 40:
+                self.x += enemy_speed
+
+            else:
+                self.x -= enemy_speed
+
+            if self.y > pyxel.height - 1:
+                self.is_alive = False
+
+            self.y -= enemy_speed
+
+        self.counter_y += enemy_speed
+
+        return None, False
+
+    def draw(self):
+        pyxel.rect(self.x, self.y, self.w, self.h, self.color)
+
+    def update(self):
+        return self.move()
