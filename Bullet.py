983dd6687d6110422da6b201f9b5227f9e3c194c
cherrnyssheva <117863274+cherrnyssheva@users.noreply.github.com>
2022-11-30 20:55:15 +0100

    added enemy bullet class

--- Bullet.py
+++ Bullet.py
@@ -1,5 +1,10 @@
 import pyxel
 
+height_bullet = 2
+width_bullet = 1
+bullet_speed = 2
+bullets = []
+
 
 class Bullet:
 
@@ -9,8 +14,8 @@ class Bullet:
 
         self.speed = speed
 
-        self.height = 2
-        self.width = 1
+        self.height = height_bullet
+        self.width = width_bullet
 
     def move(self):
         if self.y - self.height > 0:
@@ -25,3 +30,27 @@ class Bullet:
 
     def update(self):
         return self.move()
+
+
+class BulletEnemy():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.w = width_bullet
+        self.h = height_bullet
+        self.is_alive = True
+        bullets.append(self)
+
+    def move(self):
+        self.y += bullet_speed
+        if self.y + self.h - 1 < 0:
+            self.is_alive = False
+        return None, False
+
+    def draw(self):
+        pyxel.rect(self.x, self.y, self.w, self.h, 5)
+
+    def update(self):
+        return self.move()
+
