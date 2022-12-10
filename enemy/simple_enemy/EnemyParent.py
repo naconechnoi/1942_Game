a1bc27e237822046d0d60cb4d1320994e349ae75
anastasiia <anastasiia_sadova@knu.ua>
2022-12-10 13:40:30 +0100

    setters for simple enemies added

--- enemy/simple_enemy/EnemyParent.py
+++ enemy/simple_enemy/EnemyParent.py
@@ -15,6 +15,54 @@ class EnemyParent:
         self.counter_y = 0
         self.gun = EnemyGun(1)
 
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def enemy_speed(self):
+        return self.__enemy_speed
+
+    @enemy_speed.setter
+    def enemy_speed(self, enemy_speed):
+        self.__enemy_speed = enemy_speed
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, color):
+        self.__color = color
+
     def update(self, boomerang):
         raise Exception
 
