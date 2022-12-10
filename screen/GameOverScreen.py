aad683707dff45986cbd51607c2b14a9857c60e8
anastasiia <anastasiia_sadova@knu.ua>
2022-12-10 18:06:05 +0100

    screen changes fixed

--- screen/GameOverScreen.py
+++ screen/GameOverScreen.py
@@ -6,11 +6,11 @@ from screen.Screen import Screen
 class GameOverScreen(Screen):
 
     def update(self, boomerang):
-        if pyxel.btn(pyxel.KEY_SPACE):
+        if pyxel.btn(pyxel.KEY_MINUS):
             boomerang.screen = self.next_screen
 
     def draw(self):
         pyxel.cls(12)
-        pyxel.text(50, 70, "-GAME OVER-", pyxel.frame_count % 12)
-        pyxel.rectb(38, 63, 40, 20, pyxel.frame_count % 12)
-        pyxel.text(30, 135, "- PRESS ENTER -", pyxel.frame_count % 3)
+        pyxel.text(40, 70, "-GAME OVER-", pyxel.frame_count % 12)
+        pyxel.rectb(25, 63, 70, 20, pyxel.frame_count % 12)
+        pyxel.text(26, 135, """- PRESS  " - " TO -\n    START AGAIN""", pyxel.frame_count % 3)
